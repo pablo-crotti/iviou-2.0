d895f83a5cd0a3910165a2dc52182c3fc381b6ea
pablo.crotti <pablo.crotti@village-elargi.ch>
2025-07-08 06:38:32 +0200

    created server

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`App running at http://localhost:${port}`)
+})
